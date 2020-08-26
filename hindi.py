370d655af1240b06f7fbbd15922f1435e2212266
Your Name <you@example.com>
2020-08-27 00:59:56 +0530

    update

--- hindi.py
+++ hindi.py
@@ -2,4 +2,8 @@ Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (In
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("kya chal raha hn")
 kya chal raha hn
+>>> print("what is you name?")
+what is you name?
+>>> print("HI there")
+HI there
 >>> 
